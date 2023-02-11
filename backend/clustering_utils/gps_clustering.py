4c0a9a5f6fff3726d34a0b0e0e11311a51fd4d85
anirudh chebolu <ani1005sai@gmail.com>
2023-02-11 19:40:33 +0530

    clustering base classes

---
+++ backend/clustering_utils/gps_clustering.py
@@ -0,0 +1,39 @@
+from sklearn.cluster import DBSCAN
+import numpy as np
+class DBscanGps:
+    eps = 5
+    min_samples = 2
+    data = []
+    
+    def new_data(self,location):
+        self.data.append(location)
+        self.dbscan_clustering()
+
+    def push_negatives_to_end(lst):
+        return [x for x in lst if x != -1] + [x for x in lst if x == -1]
+
+    def dbscan_clustering(self):
+        dbscan = DBSCAN(eps = self.eps, min_samples=self.min_samples)
+        dbscan.fit(np.array(self.data))
+        labels = dbscan.labels_
+        vec = []
+        neg_vec = []
+        for i in range(len(labels)):
+            if labels[i] == -1:
+                neg_vec.append(self.data[i])
+            else:
+                vec.append(self.data[i])
+        vec.extend(neg_vec)
+        self.data = vec
+        print(labels)
+
+
+if __name__ == "__main__":
+    dbscan = DBscanGps()
+    dbscan.new_data([6,9])
+    dbscan.new_data([6,10])
+    dbscan.new_data([20,21])
+    dbscan.new_data([2,2])
+    dbscan.new_data([3,1])
+    dbscan.new_data([20,20])
+    #dbscan.new_data([21,21])
---
+++ backend/clustering_utils/range_clustering.py
@@ -0,0 +1,14 @@
+class RangeClustering:
+    division_parameter = 4
+    range = (0,10)
+
+    def set_division_parameter(self,division_parameter):
+        self.division_parameter = division_parameter
+
+    def set_range(self,range):
+        self.range = range
+    
+    def new_data(self,data):
+        width = max(self.range)/self.division_parameter
+        return data//width
+            
