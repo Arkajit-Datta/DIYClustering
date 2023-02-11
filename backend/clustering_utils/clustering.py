f3abe64ea278468205fec4af517e75964b0c92ac
Arkajit-Datta <arkajitdatta2010@gmail.com>
2023-02-12 04:14:04 +0530

    bugfixes/clustering

--- backend/clustering_utils/clustering.py
+++ backend/clustering_utils/clustering.py
@@ -1,10 +1,11 @@
-from gps_clustering import DBscanGps
-from multiple_classifer import MultipleClustering
-from range_clustering import RangeClustering
-from sentence_clustering import DBscanSentence
+from clustering_utils.gps_clustering import DBscanGps
+from clustering_utils.multiple_classifer import MultipleClustering
+from clustering_utils.range_clustering import RangeClustering
+from clustering_utils.sentence_clustering import DBscanSentence
 import os
 import sys
 import uuid
+
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_DIR = os.path.abspath(os.path.join(TEST_DIR, "../"))
 sys.path.insert(0, PROJECT_DIR)
@@ -37,26 +38,29 @@ def cluster(event,data):
         classifier,parameter = rule
         clustering_obj = get_clustering_object(classifier,event,parent_node,i-1,parameter)
         cluster = clustering_obj.new_data(data[parameter])
-        if parameter in ["range_clustering","multiple_classifier"]:
+        if classifier in {"range_clustering","multiple_classifier"}:
             if cluster in tree_info[parent_node].keys():
                 update_data_point(event,i,tree_info[parent_node][cluster],data["_id"])
             else:
-                tree_info[parent_node][cluster] = str(uuid.uuid4())
-                if i==len(rules)-1:
-                    pass
-                else:
+                tree_info[parent_node][cluster] = str(uuid.uuid1())
+                if i != len(rules) - 1:
                     tree_info[tree_info[parent_node][cluster]] = {}
                     update_data_point(event,i,tree_info[parent_node][cluster],data["_id"])
             parent_node = tree_info[parent_node][cluster]
         else:
-            data = get_children_from_tree_node(event, parent_node, i-1)
-            for i,x in enumerate(cluster):
-                if x in tree_info[parent_node].keys():
-                    update_data_point(event,i,tree_info[parent_node][x],data[i]["_id"])
-                else:
-                    tree_info[tree_info[parent_node][x]] = {}
-                    update_data_point(event,i,tree_info[parent_node][x],data[i]["_id"])
-
+            data1 = get_children_from_tree_node(event, parent_node, i-1)
+            
+            for j,x in enumerate(cluster):
+                if j == len(cluster)-1:
+                    if x not in tree_info[parent_node].keys():
+                        tree_info[parent_node][x] = str(uuid.uuid1())
+                    update_data_point(event,i,tree_info[parent_node][x],data["_id"])
+                    break
+                
+                if x not in tree_info[parent_node].keys():
+                    tree_info[parent_node][x] = str(uuid.uuid1())
+                update_data_point(event,i,tree_info[parent_node][x],data1[j]["_id"])
+                   
     update_tree_info(event,tree_info)
 
         
--- backend/dbUtils/create_event.py
+++ backend/dbUtils/create_event.py
@@ -1,7 +1,7 @@
 from dbUtils.mongoDb import baseDb
 
 class CreateEvent(baseDb):
-    def __init__(self, name: str, parameters: list, rules: list) -> None:
+    def __init__(self, name: str, parameters: list, rules: list)->None:
         super().__init__()
         self.structure = {
             "name": name,
@@ -11,27 +11,29 @@ class CreateEvent(baseDb):
             "parameters": parameters, 
         }
         
-        self.insert_data(collection="events", data = self.structure)
+        self.inserted_id = self.insert_data(collection="events", data = self.structure)
         
     
 class EventCollection(baseDb):
     def __init__(self, event_name, parameters) -> None:
         super().__init__()
-        self.structure = {}.update(parameters)
+        self.structure = parameters
         self.number_of_rules = None
         self._initialise_rules(event_name=event_name)
-        self.insert_data(collection=event_name, data=self.structure)
+        self.inserted_id = self.insert_data(collection=event_name, data=self.structure)
         
     def _get_number_of_rules(self, event_name):
         data = self.find_data(collection="events", filter={"name": event_name}, find="one")
-        return data['rules']
+        return len(data['rules'])
     
     def _initialise_rules(self, event_name) -> None:
         # query rules from database
         if self.number_of_rules is None:
             self.number_of_rules = self._get_number_of_rules(event_name=event_name)
             
+        print(self.structure)
         for rule in range(self.number_of_rules):
+
             self.structure[f"assigned_cluster_{rule}"] = ""
         
         
